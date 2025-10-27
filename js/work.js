b36b364bed5cd37270b48f4b6120742a0264a92e
Vinay Begar <vinaybbegar@gmail.com>
2025-10-27 12:39:13 +0530

    stack images added

---
+++ js/work.js
@@ -0,0 +1,11 @@
+// const workItems = document.querySelectorAll(".work-item");
+
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add("visible");
+//     }
+//   });
+// });
+
+// workItems.forEach((item) => observer.observe(item));
