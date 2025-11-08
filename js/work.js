ec6c45e33c9bfef332bcb869f85ed294fffc21f0
Vinay Begar <vinaybbegar@gmail.com>
2025-11-08 19:03:54 +0530

    live link updated and GSAP added

--- js/work.js
+++ js/work.js
@@ -15,3 +15,21 @@ document.querySelectorAll(".menu-head a").forEach((link) => {
     navMenu.classList.remove("active");
   });
 });
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+// Select all project cards and animate them on scroll
+gsap.utils.toArray(".project-card").forEach((card) => {
+  gsap.from(card, {
+    y: 50, // Start 50px lower
+    opacity: 0, // Start invisible
+    duration: 0.9, // Animation lasts 0.8 seconds
+    ease: "power2.out", // Smooth easing
+    scrollTrigger: {
+      trigger: card, // The element to watch
+      start: "top 80%", // When top of card reaches 80% of viewport
+      toggleActions: "play none none none", // Play once
+    },
+  });
+});
