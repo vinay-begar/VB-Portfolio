dddfa91ce54be9db9316b9c13211c643bcefeb07
Vinay Begar <vinaybbegar@gmail.com>
2025-11-11 19:11:51 +0530

    Meta tags added

--- js/contact.js
+++ js/contact.js
@@ -14,3 +14,20 @@ document.querySelectorAll(".menu-head a").forEach((link) => {
     navMenu.classList.remove("active");
   });
 });
+
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    const subject = `New message from ${name}`;
+    const body = `Name: ${name}%0AEmail: ${email}%0AMessage: ${message}`;
+
+    // Change this to your email:
+    window.location.href = `mailto:vinaybbegar@gmail.com?subject=${subject}&body=${body}`;
+  });
+
